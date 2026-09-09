69c92c3e0ba07032410f78e87956057eeedd2177
Jacob Lohman <146109980+TheCrusader10@users.noreply.github.com>
2026-09-09 14:25:42 -0400

    Move and update light color

--- drawstuff.js
+++ drawstuff.js
@@ -391,8 +391,8 @@ function main() {
     var imagedata = context.createImageData(w,h);
  
     // Define a rectangle in 2D with colors and coords at corners
-    var globals = { lightPos: new Vector(50,50,10),  // light over left upper rect
-                    lightCol: new Color(255,0,155)}; // light is red
+    var globals = { lightPos: new Vector(150,150,10),  // light over left upper rect
+                    lightCol: new Color(255,255,255)}; // light is red
     var tlAttribs = { diffuse: new Color(0,0,255)};    // all four rect verts blue
     var trAttribs = { diffuse: new Color(0,0,255)};
     var brAttribs = { diffuse: new Color(0,0,255)};
